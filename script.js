ed2696c516060acff848e756c9687eac5889be29
elpuntodeencuentro <56415139+elpuntodeencuentro@users.noreply.github.com>
2026-02-05 23:29:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const response = document.getElementById("response");
+const tease = document.getElementById("tease");
+
+yesBtn.addEventListener("click", () => {
+    response.textContent = "💖 Sabía que dirías que sí. Prometo hacerte feliz hoy y siempre 💖";
+    response.classList.remove("hidden");
+
+    noBtn.style.display = "none"; 
+    tease.textContent = "";        
+});
+
+
+noBtn.addEventListener("mouseover", () => {
+    const phrases = [
+        "Mi amor, te confundiste? 🙃",
+        "Colabora mi amor 😳",
+        "Ese botón no funciona, lo siento 😌",
+        "Déjate querer hermosa 🥰",
+        "Intenta otra vez 💕",
+        "No te hagas la difícil, sabes que sí quieres 😏",
+        "Mi amorrrrr 🙁",
+        "Eres perseverante, pero no tienes opción 😂",
+        "Creo que te equivocaste, verdad amor? 😅",
+        "Vamos, di que sí preciosa 🥰",
+        "Piénsalo bien guapa 🤭",
+        "El otro botón te espera bb 💖",
+        "¿Sigues creyendo que tienes opción? 😜",
+        "Yo sé tu respuesta ❤️",
+        "Mira la foto nuevamente 🫶🏻"
+    ];
+
+    tease.textContent = phrases[Math.floor(Math.random() * phrases.length)];
+
+    const x = Math.random() * 200 - 100;
+    const y = Math.random() * 200 - 200;
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
